bb6892775ab9b7081f16c451107c5a02ca050ec1
Zaharia Robert <98769661+RobertJan27@users.noreply.github.com>
2026-03-30 01:14:30 +0300

    Add: longest-repeating-substring-with-replacement - submission-24

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-24.java
@@ -0,0 +1,38 @@
+class Solution {
+
+
+
+
+    public int characterReplacement(String s, int k) {
+
+        HashMap<Character,Integer> freq = new HashMap<Character,Integer>();
+        int l=0;
+        int r=0;
+        int rez=0;
+        while(r<s.length())
+        {
+            int x= freq.getOrDefault(s.charAt(r),0);
+            freq.put(s.charAt(r),x+1);
+            int freqMax=0;
+            for (Integer ch :freq.values())
+                if(ch>freqMax)
+                freqMax=ch;
+            if((r-l-freqMax+1 ) <=k || (freqMax== r-l+1))
+            {
+                if(r-l+1>rez)
+                rez=r-l+1;
+                r++;
+            }
+            else
+            {
+                freq.put(s.charAt(r),x);
+                freq.put(s.charAt(l),freq.get(s.charAt(l))-1);
+                l++;
+            }
+            System.out.println(rez+ " ");
+        }
+
+        return rez;
+
+    }
+}
